e50ca618869a699add7a4177b8a282d290ca6767
PavelPutnik <126001615+PavelPutnik@users.noreply.github.com>
2023-12-11 19:21:11 +0300

    vtemnote

---
+++ static/script/script.js
@@ -0,0 +1,7 @@
+document.getElementById("main-action").onclick = function() {
+    document.getElementById("info").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("buy").onclick = function() {
+    document.getElementById("pricee").scrollIntoView({behavior: "smooth"});
+}
