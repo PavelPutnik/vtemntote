cbf53018b2de4d24124c714ccf298585928bd5b6
PavelPutnik <126001615+PavelPutnik@users.noreply.github.com>
2023-12-11 21:21:55 +0300

    new

--- static/script/script.js
+++ static/script/script.js
@@ -5,3 +5,23 @@ document.getElementById("main-action").onclick = function() {
 document.getElementById("buy").onclick = function() {
     document.getElementById("pricee").scrollIntoView({behavior: "smooth"});
 }
+
+document.getElementById("infom").onclick = function() {
+    document.getElementById("info").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("rulem").onclick = function() {
+    document.getElementById("rules1").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("pricem").onclick = function() {
+    document.getElementById("pricemm").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("bronm").onclick = function() {
+    document.getElementById("bronmm").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("book").onclick = function() {
+    document.getElementById("bronmm").scrollIntoView({behavior: "smooth"});
+}
